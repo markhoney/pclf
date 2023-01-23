0807483749ec727f9513c1e87fb90fc597aef380
Mark Honeychurch <mark@honeychurch.org>
2023-01-23 22:03:47 +1300

    Moved project files to Tiled folder

--- 20 Enemies.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="20 Enemies" tilewidth="32" tileheight="32" tilecount="285" columns="19">
- <image source="Pixel Adventure 1/Free/20 Enemies.png" width="630" height="500"/>
-</tileset>
