d436d2ecf58a7c9a73065538ce803b6fb901fcc4
Mark Honeychurch <mark@honeychurch.org>
2023-01-23 22:03:01 +1300

    Tried Tiled

---
+++ Tiled/Furniture.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Furniture" tilewidth="380" tileheight="256" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="2">
+  <image width="380" height="256" source="assets/images/background/cake.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="assets/images/furniture/table.png"/>
+ </tile>
+</tileset>
---
+++ Tiled/cakes_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="cakes_2" tilewidth="128" tileheight="128" tilecount="16" columns="4">
+ <image source="cakes_2.jpg" trans="e5d8d2" width="512" height="512"/>
+</tileset>
